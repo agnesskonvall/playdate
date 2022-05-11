0d24754be422e61a7bcc7e1d4e4726a0e38de4ae
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2022-05-11 11:08:13 +0200

    positioned top menu buttons

--- main.js
+++ main.js
@@ -11,12 +11,25 @@ let computer_button = PIXI.Sprite.from("/sprites/computer_button.png");
 let food_button = PIXI.Sprite.from("/sprites/forkandknife.png");
 let sleep_button = PIXI.Sprite.from("/sprites/moon.png");
 
-beer_button.x = 150;
-beer_button.y = 150;
+beer_button.x = 330;
+beer_button.y = 282;
+
+computer_button.x = 265;
+computer_button.y = 285;
+
+food_button.x = 120;
+food_button.y = 282;
+
+sleep_button.x = 190;
+sleep_button.y = 282;
+
+
 
 app.stage.addChild(background);
 let sprite = yrgonaut_neutral;
-
+app.stage.addChild(computer_button);
+app.stage.addChild(sleep_button)
+app.stage.addChild(food_button);
 app.stage.addChild(beer_button);
 
 app.stage.addChild(sprite);
