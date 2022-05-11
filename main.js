0ffeda384ba7078fcdb0d747938e9369433f6963
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2022-05-11 10:59:31 +0200

    added sprites to app

--- main.js
+++ main.js
@@ -5,10 +5,19 @@ let app = new PIXI.Application({ width: 500, height: 700, backgroundColor: 0xfff
 appDiv.appendChild(app.view);
 
 let background = PIXI.Sprite.from("/sprites/yrgotchi_base.png");
-let yrgonaut_neutral = PIXI.Sprite.from("/sprites/yrgonaut_neutral.png")
+let yrgonaut_neutral = PIXI.Sprite.from("/sprites/yrgonaut_neutral.png");
+let beer_button = PIXI.Sprite.from("/sprites/beermug.png");
+let computer_button = PIXI.Sprite.from("/sprites/computer_button.png");
+let food_button = PIXI.Sprite.from("/sprites/forkandknife.png");
+let sleep_button = PIXI.Sprite.from("/sprites/moon.png");
+
+beer_button.x = 150;
+beer_button.y = 150;
 
 app.stage.addChild(background);
 let sprite = yrgonaut_neutral;
+
+app.stage.addChild(beer_button);
 
 app.stage.addChild(sprite);
 
