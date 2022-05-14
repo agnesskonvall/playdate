daadb7b5e1e7db409cfa52e4d5a777bb0a0da035
Deliciaes <ceramstedt@gmail.com>
2022-05-14 17:11:47 +0200

    added classes and loader
    converted sprite in to classes with constructors, and added a loader so that the sprites are loaded before the application runs.

--- main.js
+++ main.js
@@ -1,121 +1,127 @@
 import './style.css';
 import * as PIXI from "pixi.js";
-import { Renderer } from 'pixi.js';
+import { graphicsUtils, Renderer } from 'pixi.js';
+
+
 const appDiv = document.getElementById('app');
-window.onload = function () {
-let app = new PIXI.Application({ width: 500, height: 700, backgroundColor: 0xffffff});
+let app;
+let yrgonaut;
+let eat_button;
+let sleep_button;
+let computer_button;
+let beer_button;
+let legstretch_button;
+let stack_overflow_button;
+
+class Yrgonaut extends PIXI.Sprite {
+  constructor(x = 0, y = 0, texture, mood) {
+    super(texture);
+    this.x = x;
+    this.y = y;
+    this.mood = mood;
+  }
+  idle() {
+
+  }
+}
+
+class Menu_Item extends PIXI.Sprite {
+  constructor(x = 0, y = 0, texture, interactive, buttonMode) {
+    super(texture);
+    this.x = x;
+    this.y = y;
+    this.interactive = interactive
+    this.buttonMode = buttonMode
+  }
+}
+
+window.onload = function() {
+app = new PIXI.Application(
+  {
+    width: 500,
+    height: 700,
+    transparent: true
+  }
+);
 appDiv.appendChild(app.view);
 
-let background = PIXI.Sprite.from("/sprites/yrgotchi_base.png");
 
-//height på sprite bilden ska vara 120px
-let yrgonaut_neutral = PIXI.Texture.from("/sprites/yrgonaut_neutral.png");
-let yrgonaut_happy = PIXI.Texture.from("/sprites/yrgonaut_happy.png")
+//preload sprites location:
+app.loader.baseUrl = "sprites";
+//add assets to preload here:
+app.loader
+  .add("yrgotchi_base", "yrgotchi_base.png")
+  //icons:
+  .add("beer", "beer_mug.png")
+  .add("computer", "computer.png")
+  .add("fork_and_knife", "fork_and_knife.png")
+  .add("legstretch", "legstretch.png")
+  .add("moon", "moon.png")
+  .add("stack_overflow", "stack_overflow.png")
+  //effects:
+  .add("pizza", "pizza.png")
+  //character:
+  .add("yrgonaut_neutral", "yrgonaut_neutral.png")
+  .add("yrgonaut_happy", "yrgonaut_happy.png")
 
-let beer_button = PIXI.Sprite.from("/sprites/beermug.png");
-let computer_button = PIXI.Sprite.from("/sprites/computer_button.png");
-let food_button = PIXI.Sprite.from("/sprites/forkandknife.png");
-let sleep_button = PIXI.Sprite.from("/sprites/moon.png");
-let stretch_button = PIXI.Sprite.from("/sprites/legstretch.png");
-let stack_button = PIXI.Sprite.from("/sprites/stack_overflow.png");
-let pizza = PIXI.Sprite.from("/sprites/pizza.png");
+  app.loader.onProgress.add(loadingProgress);
+  app.loader.onComplete.add(loadingSuccessful);
+  app.loader.onError.add(errorReport);
 
-app.stage.addChild(background);
-let yrgonaut = new PIXI.Sprite(yrgonaut_neutral);
-
-app.stage.addChild(background);
-app.stage.addChild(computer_button);
-app.stage.addChild(sleep_button)
-app.stage.addChild(food_button);
-app.stage.addChild(beer_button);
-
-yrgonaut.x = 208;
-yrgonaut.y = 335;
-
-beer_button.x = 330;
-beer_button.y = 282;
-
-computer_button.x = 265;
-computer_button.y = 285;
-
-food_button.x = 120;
-food_button.y = 282;
-
-sleep_button.x = 190;
-sleep_button.y = 282;
-
-stretch_button.x = 118;
-stretch_button.y = 462;
-
-stack_button.x = 185;
-stack_button.y = 464;
-
-pizza.x = 270;
-pizza.y = 350;
-
-
-food_button.interactive = true;
-food_button.buttonMode = true;
-//pointerdown fungerar för både mus och touch
-food_button.on('pointerdown', eat);
-// food_button.on('mouseup', onButtonUp);
-
-
-app.stage.addChild(background);
-app.stage.addChild(stack_button);
-app.stage.addChild(computer_button);
-app.stage.addChild(stretch_button);
-app.stage.addChild(sleep_button)
-app.stage.addChild(food_button);
-app.stage.addChild(beer_button);
-app.stage.addChild(yrgonaut);
-app.stage.addChild(pizza);
-pizza.visible=false;
-
-let timeout;
-let counter = 0;
-
- function eat() {
-  pizza.visible=true;
-  //  timeout = setTimeout(function(yrgonaut) {
-  //   yrgonaut.texture = yrgonaut_neutral;
-  // }, 3000, yrgonaut);
-
-let i = setInterval(function(){
-  yrgonaut.texture = yrgonaut_happy;
-
-  timeout = setTimeout(function(yrgonaut) {
-      yrgonaut.texture = yrgonaut_neutral;
-    }, 500, yrgonaut);
-    counter++;
-    if(counter === 4) {
-      yrgonaut.texture = yrgonaut_neutral;
-      pizza.visible=false;
-        clearInterval(i);
-        counter = 0;
-    }
-}, 1000);
-
- }
-
-//  function onButtonUp() {
-//    timeout = setTimeout(function(yrgonaut) {
-//      yrgonaut.texture = yrgonaut_neutral;
-//    }, 3000, yrgonaut);
-//  }
-
-
-
-// function feed() {
-//   yrgonaut.setTexture(yrgonaut_happy);
-// }
-
-
-
+  app.loader.load();
 
 }
-// let elapsed = 0.0;
-// app.ticker.add((delta) => {
-//   elapsed += delta;
-//   sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
-// });
+
+
+function loadingProgress(e) {
+  console.log(e.progress);
+}
+
+function errorReport(e) {
+  console.error("error: " + e.message);
+}
+
+function loadingSuccessful() {
+  console.log("loading complete");
+  //add background:
+  let background = new PIXI.Sprite.from(app.loader.resources.yrgotchi_base.texture)
+  app.stage.addChild(background);
+  //create menu:
+  create_Menu();
+  //create yrgonaut:
+  create_Yrgonaut();
+  app.ticker.add(gameLoop);
+}
+function gameLoop(delta) {
+}
+
+function create_Menu() {
+  eat_button = new Menu_Item(120, 283, app.loader.resources["fork_and_knife"].texture, true, true);
+  sleep_button = new Menu_Item(190, 283, app.loader.resources["moon"].texture, true, true);
+  computer_button = new Menu_Item(265, 285, app.loader.resources["computer"].texture, true, true);
+  beer_button = new Menu_Item(330, 282, app.loader.resources["beer"].texture, true, true);
+  legstretch_button = new Menu_Item(118, 462, app.loader.resources["legstretch"].texture, true, true);
+  stack_overflow_button = new Menu_Item(185, 464, app.loader.resources["stack_overflow"].texture, true, true);
+  app.stage.addChild(eat_button);
+  app.stage.addChild(sleep_button);
+  app.stage.addChild(computer_button);
+  app.stage.addChild(beer_button);
+  app.stage.addChild(legstretch_button);
+  app.stage.addChild(stack_overflow_button);
+}
+
+function create_Yrgonaut() {
+  yrgonaut = new Yrgonaut(235, 390, app.loader.resources["yrgonaut_neutral"].texture, "neutral");
+  yrgonaut.anchor.set(0.5);
+  app.stage.addChild(yrgonaut);
+  //console.log(yrgonaut);
+}
+
+    function eat() {
+
+      yrgonaut.texture = app.loader.resources["yrgonaut_happy"].texture
+
+      yrgonaut.texture = app.loader.resources["yrgonaut_neutral"].texture
+    }
+
+
