96915449cd4bcff76ca55fe2d3fa8743b3f1ba9d
Deliciaes <ceramstedt@gmail.com>
2022-05-19 13:50:12 +0200

    prepare for react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import ScriptsComponent from './components/ScriptsComponent/ScriptsComponent';
+import { useState } from "react";
+import "./App.css";
+import ScriptsComponent from "./components/ScriptsComponent/ScriptsComponent";
 
 function App() {
   return (
