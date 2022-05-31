1b7d5d630db2109159f630f127c6a95bda9bdf5b
Deliciaes <ceramstedt@gmail.com>
2022-05-31 12:51:06 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,12 @@ import Main from './pages/Main';
 function App() {
   return (
     <div className="App">
-      <div className="loading">Loading...</div>
-      <div className="game"></div>
+      <div className="loading">
+        .......Loading.......
+        <br />
+        (desktop support only)
+      </div>
+      <div className="gasme"></div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Main />} />
